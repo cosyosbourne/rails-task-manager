7a5274abd88121629b8f97f731c5c1784fbc0cc1
Titouan Andritsos <titouan.andritsos@hotmail.fr>
2022-08-16 16:17:34 +0200

    rails task manager back-end files done

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,21 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # Display all
+  get 'tasks', to: 'tasks#index'
+
+  # Create
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+
+  # Read one
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # Update
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # Destroy
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
---
+++ db/migrate/20220816103340_change_default_value.rb
@@ -0,0 +1,5 @@
+class ChangeDefaultValue < ActiveRecord::Migration[7.0]
+  def change
+    change_column :tasks, :completed, :boolean, default: false
+  end
+end
